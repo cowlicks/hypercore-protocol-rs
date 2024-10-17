643212b6f455aad85448b1e4627ac3b7d4969d79
Blake Griffith <blake.a.griffith@gmail.com>
2024-10-17 17:31:12 -0400

    rm custom debug impls

--- src/protocol.rs
+++ src/protocol.rs
@@ -134,6 +134,7 @@ impl fmt::Debug for State {
 }
 
 /// A Protocol stream.
+#[derive(Debug)]
 pub struct Protocol<IO: AsyncWrite + AsyncRead + Send + Unpin + 'static> {
     write_state: WriteState,
     read_state: ReadState,
@@ -148,26 +149,6 @@ pub struct Protocol<IO: AsyncWrite + AsyncRead + Send + Unpin + 'static> {
     outbound_tx: Sender<Vec<ChannelMessage>>,
     keepalive: Delay,
     queued_events: VecDeque<Event>,
-    //channel_messages: (Sender<Message>, Receiver<Message>),
-}
-
-impl<IO: AsyncWrite + AsyncRead + Send + Unpin + 'static> std::fmt::Debug for Protocol<IO> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Protocol")
-            .field("write_state", &self.write_state)
-            .field("read_state", &self.read_state)
-            .field("state", &self.state)
-            .field("options", &self.options)
-            .field("handshake", &self.handshake)
-            .field("channels", &self.channels)
-            .field("command_rx", &self.command_rx)
-            .field("command_tx", &self.command_tx)
-            .field("outbound_rx", &self.outbound_rx)
-            .field("outbound_tx", &self.outbound_tx)
-            .field("keepalive", &self.keepalive)
-            .field("queued_events", &self.queued_events)
-            .finish()
-    }
 }
 
 impl<IO> Protocol<IO>
@@ -195,7 +176,6 @@ where
             outbound_rx,
             keepalive: Delay::new(Duration::from_secs(DEFAULT_KEEPALIVE as u64)),
             queued_events: VecDeque::new(),
-            //channel_messages: async_channel::unbounded(),
         }
     }
 
